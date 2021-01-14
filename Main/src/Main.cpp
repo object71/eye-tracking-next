00191aa1970e9672276d61d0ef9a5729f08bb6ae
Hristo Stamenov <capitane71@gmail.com>
2021-01-14 09:20:34 +0200

    Working build of opencv

--- Main/src/Main.cpp
+++ Main/src/Main.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
 
 int main()
 {
 	std::cout << "Application started" << std::endl;
 	
-//	cv::Mat peaceImage = cv::imload("peace.jpg", cv::CV_LOADIMAGE_UNCHANGED);
-//	imshow("Peace", peaceImage);
-//	cv::waitKey();
+	cv::Mat peaceImage = cv::imread("./data/peace.jpg", cv::IMREAD_UNCHANGED);
+	
+	if(peaceImage.empty()) return 1;
+	
+	cv::imshow("Peace", peaceImage);
+	cv::waitKey(0);
 
     return 0;
 }
--- README.md
+++ README.md
@@ -1,3 +1,12 @@
+# Setup
+
+To be able to build the project you would need to:
+
+```
+./vcpkg/bootstrap-vcpkg.bat
+./vcpkg/vcpkg.exe install opencv:x63-windows
+```
+
 # Build
 
 git submodule --init --recurse
