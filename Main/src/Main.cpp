9f643e1737d9a603f463505c5f9abd53e6b469f6
Hristo Stamenov <capitane71@gmail.com>
2021-01-17 22:01:33 +0200

    Working opencv code

--- Main/src/Main.cpp
+++ Main/src/Main.cpp
@@ -3,17 +3,65 @@
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
+#include <spdlog/spdlog.h>
+#include <chrono>
 
 int main()
 {
-	std::cout << "Application started" << std::endl;
+	spdlog::info("Application started");
+	spdlog::set_level(spdlog::level::debug);
 	
-	cv::Mat peaceImage = cv::imread("./data/peace.jpg", cv::IMREAD_UNCHANGED);
+	//cv::CAP_DSHOW or cv::CAP_MSMF or cv::CAP_V4L
+	cv::VideoCapture capture;
 	
-	if(peaceImage.empty()) return 1;
+	capture.open(0, cv::VideoCaptureAPIs::CAP_MSMF);
+	if (!capture.isOpened())
+	{
+		spdlog::critical("Couldn't open capture device!");
+		return 1;
+	}
 	
-	cv::imshow("Peace", peaceImage);
-	cv::waitKey(0);
-
-    return 0;
+	spdlog::info("Camera captured successfully");
+	
+	cv::Mat frame;
+	using namespace std::chrono;
+	
+	time_point<steady_clock> previousTime = steady_clock::now();
+	float lag = 0.0f;
+	const float desiredFrameRate = 0.016f;
+	
+	while (true)
+	{
+		time_point<steady_clock> currentTime = steady_clock::now();
+		auto duration = currentTime - previousTime;
+		const float delta = duration_cast<nanoseconds>(duration).count() / 1000000000.f;
+		lag += delta;
+		
+		previousTime = currentTime;
+		
+		if (lag >= desiredFrameRate)
+		{
+			if (cv::waitKey(25) == 27)
+			{
+				break;
+			}
+			
+			// Update
+			if (capture.read(frame))
+			{
+				if (frame.empty())
+				{
+					spdlog::critical("Frame is empty!");
+					break;
+				}
+				
+				cv::imshow("Camera", frame);
+				
+				lag = 0.0f;
+			}
+		}
+	}
+	
+	capture.release();
+	return 0;
 }
--- README.md
+++ README.md
@@ -5,8 +5,9 @@ To be able to build the project you would need to:
 ```
 ./vcpkg/bootstrap-vcpkg.bat
 ./vcpkg/vcpkg.exe install opencv:x64-windows
+./vcpkg/vcpkg.exe install spdlog:x64-windows
 
-cmake -S . -B build
+cmake -S . -B build "-DCMAKE_TOOLCHAIN_FILE=./vcpkg/scripts/buildsystems/vcpkg.cmake"
 ```
 
 # Build
